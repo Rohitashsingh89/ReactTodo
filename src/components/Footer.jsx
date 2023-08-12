63ec50f0a4b0c1ac2e062450330d345408c95473
Rohitash Singh <rohitashmtr2002@gmail.com>
2023-08-13 03:32:54 +0530

    changes in footer and header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 function Footer() {
   return (
     <>
-      <div className='mt-5 d-flex justify-content-center bg-primary pt-4 pb-3'>
-        <div>Copyright &copy; Not Reserved!! </div>
-        <p className='pl-2'> Designed By <a href='https://rohitashsingh.vercel.app' className='text-light'>Rohitash Singh</a> </p>
+      <div class="footer">
+        <span>Copyright &copy; Not Reserved!!</span>
+        <span class="pl-2">Designed By <a href='https://rohitashsingh.vercel.app' class='text-light mt-4'>Rohitash Singh</a></span>
       </div>
+
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 function Header() {
   return (
     <>
-      <div><h1 className='text-center py-3 mb-5 fw-bold fs-1 bg-primary'>STACK TAKER</h1></div>
+      <header>
+        <div><h1 className='text-center '>STACK TAKER</h1></div>
+      </header>
     </>
   )
 }
